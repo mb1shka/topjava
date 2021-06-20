af8eead4f4810f99a6519dbc16cd83416e2f20d0
mb1shka <ekaterina.mishakova.a@gmail.com>
2021-06-21 01:27:30 +0300

    applied implemintation db patch

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -14,7 +14,11 @@ import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @ContextConfiguration("classpath:spring/spring-app.xml")
+//аннотация говорит, что мы из xml файла берем контекст (данные)
+
 @RunWith(SpringRunner.class)
+//через аннотацию хапускается Спринг, как я поняла
+
 public class InMemoryAdminRestControllerSpringTest {
 
     @Autowired
