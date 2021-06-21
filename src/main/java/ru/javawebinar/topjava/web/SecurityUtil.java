82d8b4312c6a9eb9c1320200fe4d875896876c90
mb1shka <ekaterina.mishakova.a@gmail.com>
2021-06-21 21:50:42 +0300

    applied servlet fix patch

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -1,12 +1,14 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
     //заглушка для security (security пройдем потом)
 
-    private static int id = 1;
+    private static int id = AbstractBaseEntity.START_SEQ;
 
     public static int authUserId() {
         return id;
