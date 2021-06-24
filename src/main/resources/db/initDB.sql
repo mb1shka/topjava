7a6e7821702b42052a4a8f3004ef96ee7e500857
mb1shka <ekaterina.mishakova.a@gmail.com>
2021-06-24 06:25:38 +0300

    fixed HW

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 
 DROP SEQUENCE IF EXISTS global_seq;
@@ -42,9 +42,9 @@ CREATE TABLE meals
     id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
     user_id     INTEGER   NOT NULL,
     date_time   TIMESTAMP NOT NULL,
-    description VARCHAR   NOT NULL,
-    calories    INTEGER   NOT NULL,
-    CONSTRAINT meals_date_time UNIQUE (user_id, date_time),
+    description TEXT   NOT NULL,
+    calories    INT   NOT NULL,
+    --CONSTRAINT meals_date_time UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX meals_unique_date_time_idx ON meals (date_time);
