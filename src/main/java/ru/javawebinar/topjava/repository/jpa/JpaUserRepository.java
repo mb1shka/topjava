a80d3bd746a3e3bf003fb6f5d98153e507d53cd8
mb1shka <ekaterina.mishakova.a@gmail.com>
2021-06-29 07:55:49 +0300

    applied named query and transaction patch

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserRepository.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.repository.jpa;
 
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
@@ -10,6 +12,7 @@ import javax.persistence.Query;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaUserRepository implements UserRepository {
 
 /*
@@ -25,37 +28,54 @@ public class JpaUserRepository implements UserRepository {
     private EntityManager em;
 
     @Override
+    @Transactional
     public User save(User user) {
         if (user.isNew()) {
             em.persist(user);
+            //save new entity
+
             return user;
         } else {
             return em.merge(user);
+            //merge with entity that already exists
         }
     }
 
     @Override
     public User get(int id) {
         return em.find(User.class, id);
+        //find user by id
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
 
 /*      User ref = em.getReference(User.class, id);
         em.remove(ref);
-*/
+        //reference - достает ссылку на объект, но не весь объект
+
         Query query = em.createQuery("DELETE FROM User u WHERE u.id=:id");
-        return query.setParameter("id", id).executeUpdate() != 0;
+        //запросы пишутся на JPQL - язык запроса к ORM, где мы оперируем объектами, а не колонками БД
+        return query.setParameter("id", id).executeUpdate() != 0;*/
+
+        return em.createNamedQuery(User.DELETE)
+                //задаем константу, в константе уже задаем операцию
+                //константы задаются в нашем случае в User
+                .setParameter("id", id)
+                .executeUpdate() != 0;
     }
 
     @Override
     public User getByEmail(String email) {
-        return null;
+        List<User> users = em.createNamedQuery(User.BY_EMAIL, User.class)
+                .setParameter(1, email)
+                .getResultList();
+        return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return em.createNamedQuery(User.ALL_SORTED, User.class).getResultList();
     }
 }
